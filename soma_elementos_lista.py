68a421f631af352f476c7195d4312795ef773422
Vinícius-Freire <120794204+vinicius-freire-oliveira@users.noreply.github.com>
2024-06-13 11:20:40 -0300

    Add files via upload
    Calcula a soma dos elementos da lista atual

---
+++ soma_elementos_lista.py
@@ -0,0 +1,8 @@
+# Lista de listas de números inteiros
+lista_de_listas = [[4, 6, 5, 9], [1, 0, 7, 2], [3, 4, 1, 8]]
+
+# Iteração sobre cada lista dentro de lista_de_listas
+for lista in lista_de_listas:
+    # Calcula a soma dos elementos da lista atual
+    print(sum(lista))
+
